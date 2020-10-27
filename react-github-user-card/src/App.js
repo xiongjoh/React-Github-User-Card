b800c151d83b637e81cc155473e1817611ebebe1
unknown <xiongjoh@gmail.com>
2020-10-27 15:13:16 -0500

    add function to get and set state for user and followers

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -7,14 +7,52 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      user:[],
+      user:{},
     }
+  }
+
+  fetchUserData = (username) => {
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(res => {
+      // console.log(res.data)
+      this.setState({
+        user:res.data
+      })
+      return res.data.followers_url
+    })
+    .then(followerURL => {
+      // console.log(followerURL)
+      axios.get(followerURL)
+      .then(res => {
+        // console.log(res.data)
+        this.setState({
+          user:{...this.state.user,
+          follower_data:res.data}
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  }
+
+  componentDidMount() {
+    this.fetchUserData('xiongjoh')
   }
 
   render() {
     return (
       <div className="main container">
-        <p>Hello</p>
+        <h2>Name: {this.state.user.name}</h2>
+        <h4>Follower Names: </h4>
+        <p>
+          {Array.isArray(this.state.user.follower_data) 
+            && this.state.user.follower_data.map(follower => follower.login).join(', ')}
+        </p>
       </div>
     )
   }
